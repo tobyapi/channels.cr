8c3f58c7ca514e8bb46ae1bbc5974684a2f4f570
TobiasGSmollett <landofscala@gmail.com>
2018-06-25 01:23:12 +0900

    bug fix

--- spec/ring_channel_spec.cr
+++ spec/ring_channel_spec.cr
@@ -3,10 +3,11 @@ require "./spec_helper"
 include Channels
 
 describe Channels do
-  it "ring" do
+  it "ring" do    
     ring_channel = RingChannel(Int32).new(10)
-    puts "came"
-    (1..1000).each { |i| ring_channel.send i }
+    (1..1000).each { |i| 
+        #puts "#{i} #{ring_channel.full?}"
+      ring_channel.send i }
     (991..1000).each do |i|
       true.should eq(i == ring_channel.receive)
     end
--- src/channels/ring_channel.cr
+++ src/channels/ring_channel.cr
@@ -1,8 +1,18 @@
 module Channels
-  class RingChannel(T) < Channel::Buffered(T)   
+  class RingChannel(T) < Channel::Buffered(T)
+    def initialize(capacity)
+      super(capacity)
+    end
+
     def send(value : T)
-      @queue.shift? if full? 
-      previous_def send value
+      @queue.shift if full?
+
+      raise_if_closed
+      @queue << value
+      Scheduler.enqueue @receivers
+      @receivers.clear
+
+      self
     end
   end
 end
